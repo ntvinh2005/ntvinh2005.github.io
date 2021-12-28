f47b6d1c19122cefb59ebdfb214f4f0587ff19a8
The Vinh <89290241+ntvinh2005@users.noreply.github.com>
2021-12-28 11:35:41 +0700

    adding style css and completing UI

--- Projects/Convert_markdown_into_html/test_convert.js
+++ Projects/Convert_markdown_into_html/test_convert.js
@@ -511,3 +511,16 @@ function checkForExistence(array, item) {
     });
     return isExist
 }
+
+const copyToClipboard = () => {
+    const el = document.createElement('textarea');
+    el.value = document.querySelector('#output').textContent;
+    console.log(el.value)
+    el.setAttribute('readonly', '');
+    el.style.position = 'absolute';
+    el.style.left = '-9999px';
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+  };
